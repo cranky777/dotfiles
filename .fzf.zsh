0acf28d5a01b6e8e62eb16c5dc9a5cbe8b835ed6
cranky777 <cranky777@gmail.com>
2026-03-20 09:18:24 +0900

    modiry fzf

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,7 +1,7 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/bot/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/bot/.fzf/bin"
+if [[ ! "$PATH" == *${HOME}/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}${HOME}/.fzf/bin"
 fi
 
 source <(fzf --zsh)
